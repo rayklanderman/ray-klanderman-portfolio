ee57217a44ca711809cbbfb60b1cf2fff5a7f2f0
WEBDEV <your@email.com>
2025-11-14 21:13:16 +0300

    feat: Update favicon to use portfolio-icon.png
    - Remove old favicon files (favicon.svg, favicon-16x16.png, etc.)
    - Update HTML to use portfolio-icon.png as the single favicon source
    - Add cache-busting parameter to prevent browser caching issues
    - Update manifest.json to use portfolio-icon.png for PWA icons

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dpg5dqpud5s"
+    "revision": "0.8lggdieefbk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
